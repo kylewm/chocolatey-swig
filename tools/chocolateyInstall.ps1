089310dd664119a041c2ee60cb9e6f35ca665631
Kyle Mahan <kyle.mahan@gmail.com>
2014-03-11 13:26:51 -0700

    Initial check-in

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,2 @@
+Install-ChocolateyZipPackage 'swig' 'http://downloads.sourceforge.net/project/swig/swigwin/swigwin-2.0.12/swigwin-2.0.12.zip?r=&ts=1394566398&use_mirror=softlayer-ams' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
